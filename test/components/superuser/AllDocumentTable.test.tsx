4515a7c44d73f95cc697a9c75ee19bda5cec6549
yui0303 <iampeter62@gmail.com>
2024-06-05 17:06:28 +0800

    chore: update snapshot

--- test/components/superuser/AllDocumentTable.test.tsx
+++ test/components/superuser/AllDocumentTable.test.tsx
@@ -12,9 +12,9 @@ it('SuperUserAllDocumnetTable renders correctly', () => {
           class="flex items-center py-4"
         >
           <input
-            class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 max-w-sm"
-            id="filter-owner"
-            placeholder="過濾所有者..."
+            class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            placeholder="搜尋"
+            type="text"
             value=""
           />
           <button
--- test/components/ui/button.test.tsx
+++ test/components/ui/button.test.tsx
@@ -6,7 +6,7 @@ it('Button renders correctly', () => {
   const { container } = render(<Button />)
   expect(container.firstChild).toMatchInlineSnapshot(`
     <button
-      class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
+      class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-blue-400 h-10 px-4 py-2"
     />
   `)
 })
